172490b50fb2236bc84a7d618d304c781c06d317
olgaKhristo <olga84h@gmail.com>
2023-07-29 19:15:10 +0100

    start of aplication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {render} from "react-dom"
+import Calendar from "react-calendar"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ReactCalendar = () =>{
+  const [date, setDate] = useState(new Date());
+  const onChange = date => {
+    setDate(date)
+  }
+  return <div>HI!!</div>
+}
+render(
+  <ReactCalendar />, document.querySelector("#root")
+)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
